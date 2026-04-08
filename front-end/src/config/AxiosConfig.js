982afaf2fc51a549c6586d1a17057f0693e1d0d2
Rafael Bernardes Sposito <rajingg7@gmail.com>
2026-04-08 18:22:54 -0300

    funcional

--- front-end/src/config/AxiosConfig.js
+++ front-end/src/config/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import axios from "axios";
 
 const AxiosConfig = axios.create({
-    baseURL: "https://seu-backend.onrender.com"
+    baseURL: "https://spotfree-v1-1.onrender.com"
 });
 
 export default AxiosConfig;
